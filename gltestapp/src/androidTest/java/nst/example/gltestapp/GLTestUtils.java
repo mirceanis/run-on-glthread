7c5a6089539755440b62420697911ea0fe679fbc
Mircea Nistor <mirceanis@gmail.com>
2016-05-07 18:46:07 +0300

    add a dummy renderer and synchronize with the GLThread

--- gltestapp/src/androidTest/java/nst/example/gltestapp/GLTestUtils.java
+++ gltestapp/src/androidTest/java/nst/example/gltestapp/GLTestUtils.java
@@ -3,38 +3,70 @@ package nst.example.gltestapp;
 import android.app.Activity;
 import android.opengl.GLSurfaceView;
 
+import java.util.concurrent.CountDownLatch;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
 /**
  * Created by mnistor on 2016-05-06.
  */
 public class GLTestUtils {
 
     private static GLTestUtils mInstance;
-
-    /**
-     * the crux
-     */
-    private GLSurfaceView mGLSurfaceView;
-
-    /**
-     * activity to hold the crux
-     */
-    private Activity mActivity;
-
+    private GL10 mGL;
 
     protected GLTestUtils() {
         //quick, quick, hide!!
     }
 
-    protected GLTestUtils(Activity activity) {
+    protected GLTestUtils(Activity activity) throws Exception {
+
         mActivity = activity;
+        activity.runOnUiThread(new Runnable() {
+            public void run() {
+                // New or different activity, set up for GL.
+                mGLSurfaceView = (GLSurfaceView) mActivity.findViewById(R.id.my_gl_surface_view);
+                if (mGLSurfaceView == null) {
+                    mGLSurfaceView = new GLSurfaceView(mActivity);
+                    mGLSurfaceView.setId(R.id.my_gl_surface_view);
+                }
+                mGLSurfaceView.setEGLContextClientVersion(2);
+
+                // Attach the renderer to the view
+                mGLSurfaceView.setRenderer(new DummyRenderer());
+                mGLSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+
+                //and the view to the activity.
+                //TODO: find a way to not destroy the content.. it may not be our activity
+                mActivity.setContentView(mGLSurfaceView);
+            }
+        });
+
+        // Wait for the renderer to get the GL context.
+        synchronized(mGlReadyLock) {
+            while (mGL == null) {
+                mGlReadyLock.wait();
+            }
+        }
     }
+
+    /**
+     * Essentially this is used to enqueue events on it's associated GLThread
+     */
+    private GLSurfaceView mGLSurfaceView;
+
+    /**
+     * An activity is used to contain the GLSurfaceView.
+     */
+    private Activity mActivity;
 
     /**
      * You gotta start somewhere.
      * This method is best called in your test setup
      * It's gonna be heavy so don't forget to {@link #release()}!
      */
-    public static synchronized void initialize(Activity activity) {
+    public static synchronized void initialize(Activity activity) throws Exception {
         mInstance = new GLTestUtils(activity);
     }
 
@@ -45,7 +77,7 @@ public class GLTestUtils {
         mInstance = null;
     }
 
-    public static void runOnGLThreadAndWait(Runnable payload) {
+    public static void runOnGLThreadAndWait(Runnable payload) throws Exception {
 
         if (mInstance == null) {
             throw new RuntimeException("have you called initialize() in your test setup() method?");
@@ -54,8 +86,44 @@ public class GLTestUtils {
         mInstance.doRunOnGLThread(payload);
     }
 
-    private void doRunOnGLThread(Runnable payload) {
-        //TODO: do the actual running
+    private void doRunOnGLThread(final Runnable payload) throws InterruptedException {
+        final CountDownLatch latch = new CountDownLatch(1);
+        mGLSurfaceView.queueEvent(new Runnable() {
+            public void run() {
+                payload.run();
+                latch.countDown();
+            }
+        });
+        latch.await();  // wait for the payload to finish
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////
+    // dummy renderer
+    ///////////////////////////////////////////////////////////////////////////
+
+
+    /**
+     * Dummy renderer, exposes the GL context
+     */
+    private static final Object mGlReadyLock = new Object();
+    private class DummyRenderer implements GLSurfaceView.Renderer {
+
+        @Override
+        public void onDrawFrame(GL10 gl) {
+
+        }
+        @Override
+        public void onSurfaceChanged(GL10 gl, int width, int height) {
+
+        }
+        @Override
+        public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+            synchronized(mGlReadyLock) {
+                mGL = gl;
+                mGlReadyLock.notifyAll();
+            }
+        }
     }
 
 }
